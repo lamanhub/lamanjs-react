3fba34d088fbb9682a93d800963294ec75deef62
akbaraditamasp <akbaraditamasp@gmail.com>
2024-09-23 10:21:43 +0700

    fix: read more text

--- src/entries/App.tsx
+++ src/entries/App.tsx
@@ -26,7 +26,7 @@ export default function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite, React or Laman.js logos to learn more
+        Click on the Laman.js or React logos to learn more
       </p>
     </>
   );
